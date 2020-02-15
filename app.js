abd756d7a9b8aa3d5fe767571f8fc6f695598f5b
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-02-15 22:30:17 +0900

    added github ribbon

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 (function(){
-  var style = "geolonia/basic"
+  var defaultStyle = "geolonia/basic"
+
+  var repoUrl = "https://github.com/%s"
+  var styleUrl = 'https://raw.githubusercontent.com/%s/master/style.json'
+  var style = ''
 
   if (location.hash) {
     var stylePath = location.hash.replace( /^#/, '' ).split( /\// )
-
-    var styleUrl = 'https://raw.githubusercontent.com/%s/master/style.json'
     style = styleUrl.replace( '%s', stylePath.join( '/' ) )
+    repo = repoUrl.replace( '%s', stylePath.join( '/' ) )
+  } else {
+    style = styleUrl.replace( '%s', defaultStyle )
+    repo = repoUrl.replace( '%s', defaultStyle )
   }
+
+  console.log(repo)
 
   var e = document.getElementById( 'map' )
   e.dataset.style = style
+
+  var a = document.getElementById( 'forkme' )
+  a.href = repo
 
   var map = new geolonia.Map( document.getElementById( 'map' ) );
 })()
