4dbb276c0ae3d22191960010d8c675f53c54d42f
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-03-06 05:54:49 +0900

    some fix

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-(function(){
-  var defaultStyle = "geolonia/basic"
+(() => {
 
-  var repoUrl = "https://github.com/%s"
-  var styleUrl = 'https://raw.githubusercontent.com/%s/master/style.json'
-  var style = ''
+  const getStyle = () => {
+    const defaultStyle = "geolonia/basic"
 
-  if (location.hash) {
-    var stylePath = location.hash.replace( /^#/, '' ).split( /\// )
-    style = styleUrl.replace( '%s', stylePath.join( '/' ) )
-    repo = repoUrl.replace( '%s', stylePath.join( '/' ) )
-  } else {
-    style = styleUrl.replace( '%s', defaultStyle )
-    repo = repoUrl.replace( '%s', defaultStyle )
+    const repoUrl = "https://github.com/%s"
+    const styleUrl = 'https://raw.githubusercontent.com/%s/master/style.json'
+
+    let url, repo
+
+    if (location.hash) {
+      const stylePath = location.hash.replace( /^#/, '' ).split( /\// )
+      url = styleUrl.replace( '%s', stylePath.join( '/' ) )
+      repo = repoUrl.replace( '%s', stylePath.join( '/' ) )
+    } else {
+      url = styleUrl.replace( '%s', defaultStyle )
+      repo = repoUrl.replace( '%s', defaultStyle )
+    }
+
+    return {url: url, repo: repo}
   }
 
-  console.log(repo)
+  const style = getStyle()
 
-  var e = document.getElementById( 'map' )
-  e.dataset.style = style
+  const e = document.getElementById( 'map' )
+  e.dataset.style = style.url
 
-  var a = document.getElementById( 'forkme' )
-  a.href = repo
+  const a = document.getElementById( 'forkme' )
+  a.href = style.repo
 
-  var map = new geolonia.Map( document.getElementById( 'map' ) );
+  const map = new geolonia.Map( document.getElementById( 'map' ) );
+
+  window.addEventListener('hashchange', () => {
+    const style = getStyle()
+    map.setStyle(style.url)
+  })
 })()
