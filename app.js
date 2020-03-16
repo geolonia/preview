3e31aaff24355c7eb617d1fc7c2eb643d6d4d526
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-03-16 21:12:26 +0900

    some fix

--- app.js
+++ app.js
@@ -2,36 +2,33 @@
 
   const getStyle = () => {
     const defaultStyle = "geolonia/basic"
-
-    const repoUrl = "https://github.com/%s"
     const styleUrl = 'https://raw.githubusercontent.com/%s/master/style.json'
 
-    let url, repo
+    let style
 
     if (location.hash) {
-      const stylePath = location.hash.replace( /^#/, '' ).split( /\// )
-      url = styleUrl.replace( '%s', stylePath.join( '/' ) )
-      repo = repoUrl.replace( '%s', stylePath.join( '/' ) )
+      if (location.hash.match(/^https:\/\//)) {
+        style = location.hash.replace( /^#/, '' )
+      } else {
+        const stylePath = location.hash.replace( /^#/, '' ).split( /\// )
+        style = styleUrl.replace( '%s', stylePath.join( '/' ) )
+      }
     } else {
-      url = styleUrl.replace( '%s', defaultStyle )
-      repo = repoUrl.replace( '%s', defaultStyle )
+      style = styleUrl.replace( '%s', defaultStyle )
     }
 
-    return {url: url, repo: repo}
+    return style
   }
 
   const style = getStyle()
 
   const e = document.getElementById( 'map' )
-  e.dataset.style = style.url
-
-  const a = document.getElementById( 'forkme' )
-  a.href = style.repo
+  e.dataset.style = style
 
   const map = new geolonia.Map( document.getElementById( 'map' ) );
 
   window.addEventListener('hashchange', () => {
     const style = getStyle()
-    map.setStyle(style.url)
+    map.setStyle(style)
   })
 })()
